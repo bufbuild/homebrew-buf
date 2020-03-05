953ba32a3fcf77e07ff889a92abdbfed131fb651
bufdev <bufdev-github@buf.build>
2020-03-05 10:42:39 -0500

    v0.7.1

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -1,15 +1,15 @@
 class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
-  version "0.7.0"
+  version "0.7.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bufbuild/buf/releases/download/v0.7.0/buf-Darwin-x86_64.tar.gz"
-    sha256 "7a3dd1dd68cd94695fa89414e1f3ff709db96faba94e644e9061828b6b6f19c1"
+    url "https://github.com/bufbuild/buf/releases/download/v0.7.1/buf-Darwin-x86_64.tar.gz"
+    sha256 "6a2b23eab3028026f404125afc47673fa9fd273b5e47e796ae76c01d6cfe06aa"
   elsif OS.linux?
-    url "https://github.com/bufbuild/buf/releases/download/v0.7.0/buf-Linux-x86_64.tar.gz"
-    sha256 "7fe0c2d1b958bc5d2434eaaac2e8278808dd22b7e595cbf2b97b0d23dd8e9d02"
+    url "https://github.com/bufbuild/buf/releases/download/v0.7.1/buf-Linux-x86_64.tar.gz"
+    sha256 "8fc435e029bb7eedbd6f96f667a126b0a0ca0289c8894bfd41bd08d91c31e336"
   end
 
   def install
