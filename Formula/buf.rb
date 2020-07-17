9fa30f3dc9d1b02dfb3101ed3cb75cb77dc58a8e
bufdev <bufdev-github@buf.build>
2020-07-17 16:07:41 -0400

    v0.20.2

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git"
-  url "https://github.com/bufbuild/buf/archive/v0.20.1.tar.gz"
-  sha256 "2c82f3b6a43fcd3daafd14cb8045ffb7244f474217cb47c0e14683d964541c55"
-  version "0.20.1"
+  url "https://github.com/bufbuild/buf/archive/v0.20.2.tar.gz"
+  sha256 "b388bac759099ae308e6232a4622b58623fb6e33795878445b458ef1df4ccdd2"
+  version "0.20.2"
 
   depends_on "go" => :build
 
