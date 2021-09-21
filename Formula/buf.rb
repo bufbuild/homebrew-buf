92fe1b6e51ba12e84d93ffb11e83fb5692b7bc0e
Koichi Shiraishi <zchee.io@gmail.com>
2021-09-22 00:53:08 +0900

    Fix bufbuild/buf head branch to main
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -1,7 +1,7 @@
 class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
-  head "https://github.com/bufbuild/buf.git"
+  head "https://github.com/bufbuild/buf.git", branch: "main"
   url "https://github.com/bufbuild/buf/archive/v1.0.0-rc1.tar.gz"
   sha256 "0d0ec20014918ac183c705183b20804e06692bbed6338608674d4591738f32bf"
   version "1.0.0-rc1"
