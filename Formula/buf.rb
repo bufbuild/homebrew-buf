c8085c3adaa6c5a234a51a39718bbeab998992a6
Doria Keung <doria.keung@gmail.com>
2022-03-21 11:33:02 -0400

    v1.1.1

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.1.0.tar.gz"
-  sha256 "fdaa543f9da7d4e2c785fa47dfde0567708432f8252ca8362dc0dc1811d902e1"
-  version "1.1.0"
+  url "https://github.com/bufbuild/buf/archive/v1.1.1.tar.gz"
+  sha256 "506a98a008a5ddbaaed5dd41ac637d9c3317ffb037ff8414baed64610c59ea80"
+  version "1.1.1"
 
   depends_on "go" => :build
 
