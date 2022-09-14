4d8857183e676aa7b0e20a799421c397e12ae920
Nick Hale <4175918+njhale@users.noreply.github.com>
2022-09-14 13:08:26 -0400

    v1.8.0

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.7.0.tar.gz"
-  sha256 "090e72d2808b4e3d641259a482931c40e77532fb3062d70c2f9b80f7f6e7e1bd"
-  version "1.7.0"
+  url "https://github.com/bufbuild/buf/archive/v1.8.0.tar.gz"
+  sha256 "8103cfbe8f4b14fe9bf39f1c2b2676f87e80771d41de51358b4ac33488a7da81"
+  version "1.8.0"
 
   depends_on "go" => :build
 
