8a7490e0b564e861ad74c50538f56ea3e9cf169c
joshcarp <joshcarp@users.noreply.github.com>
2023-06-05 20:46:04 +0000

    Update version to v1.21.0

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "A new way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.20.0.tar.gz"
-  sha256 "7aee2bfc54e022fea9c884fbc58c1c39e52586087e8da7bb0f6034d37b625f60"
-  version "1.20.0"
+  url "https://github.com/bufbuild/buf/archive/v1.21.0.tar.gz"
+  sha256 "daf5b663960ece9a0d042ad715a1134ac54b8d3963b479787b275fbf27810f6b"
+  version "1.21.0"
 
   depends_on "go" => :build
 
