a7525e5d1a8be7aac341b4624aa711a226ac4973
oliversun9 <oliversun9@users.noreply.github.com>
2024-09-18 21:34:47 +0000

    Update version to 1.42.0

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "The best way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.41.0.tar.gz"
-  sha256 "b9721906b0bfccffefe1d2c52e2b472dc212e40ff4be49c61d153132c3ce3371"
-  version "1.41.0"
+  url "https://github.com/bufbuild/buf/archive/v1.42.0.tar.gz"
+  sha256 "825e9004703c6817bd1bd2925004d2e22e55359c6c5e30cc748dc09637b0e59b"
+  version "1.42.0"
 
   depends_on "go" => :build
 
