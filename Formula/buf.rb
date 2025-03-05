1ec624b0b33685908092ad5697ec27eb0949d8eb
Edward McFarlane <emcfarlane@buf.build>
2025-03-05 19:44:00 +0100

    Add buf binary install

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -1,17 +1,41 @@
 class Buf < Formula
   desc "The best way of working with Protocol Buffers."
   homepage "https://buf.build"
-  head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.50.0.tar.gz"
-  sha256 "8ef886f4793bc76abc91da41a2ab87666bb5bfef86ddbb95e7f8240b8978c1df"
-  version "1.50.0"
+  version "1.48.0"
+  url "https://github.com/bufbuild/buf/releases/download/v#{version}/buf-Darwin-arm64"
+  sha256 "6ef7df6116c72ef3ccaa8574c9b8008389f0911c1921da4689e0abff3924a4bd"
 
-  depends_on "go" => :build
+  checksums = {
+    "darwin-arm64" => "6ef7df6116c72ef3ccaa8574c9b8008389f0911c1921da4689e0abff3924a4bd",
+    "darwin-x86_64" => "5af7155250ad8af5f0a2f5701d1df83bfece53dfe98f13b3768d808468fcdbde",
+    "linux-arm64" => "76b531e2c29be3b0ddf8b1034126b56506532b47c98b3968a49f09432536f39d",
+    "linux-x86_64" => "30cbac88a3cdda6e48dcf727f3cf9d2669d771c5d5be94f37e202c76a522b66e",
+  }
+
+  on_macos do
+    on_arm do
+      url "https://github.com/bufbuild/buf/releases/download/v#{version}/buf-Darwin-arm64"
+      sha256 checksums["darwin-arm64"]
+    end
+    on_intel do
+      url "https://github.com/bufbuild/buf/releases/download/v#{version}/buf-Darwin-x86_64"
+      sha256 checksums["darwin-x86_64"]
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/bufbuild/buf/releases/download/v#{version}/buf-Linux-aarch64"
+      sha256 checksums["linux-arm64"]
+    end
+    on_intel do
+      url "https://github.com/bufbuild/buf/releases/download/v#{version}/buf-Linux-x86_64"
+      sha256 checksums["linux-x86_64"]
+    end
+  end
 
   def install
-    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-    system "sh", "make/buf/scripts/brew.sh", ".build/brew"
-    prefix.install Dir[".build/brew/*"]
+    bin.install "buf-#{OS.kernel_name}-#{Hardware::CPU.arch}" => "buf"
   end
 
   test do
