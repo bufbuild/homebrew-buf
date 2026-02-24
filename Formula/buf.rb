7ed691dbb558b0985bc61a64d07b764224624267
doriable <doriable@users.noreply.github.com>
2026-02-24 00:01:21 +0000

    Update version to 1.66.0

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -2,9 +2,9 @@ class Buf < Formula
   desc "The best way of working with Protocol Buffers."
   homepage "https://buf.build"
   head "https://github.com/bufbuild/buf.git", branch: "main"
-  url "https://github.com/bufbuild/buf/archive/v1.65.0.tar.gz"
-  sha256 "386b49aaa2d763fa5d73ff2eb7e88fff89c6b1feacdc6b5e1badc1a173d79fbf"
-  version "1.65.0"
+  url "https://github.com/bufbuild/buf/archive/v1.66.0.tar.gz"
+  sha256 "afbe924144f3c229a1167a82e805a481f5def88cd31071e4ef7e812daeb06e27"
+  version "1.66.0"
 
   depends_on "go" => :build
 
